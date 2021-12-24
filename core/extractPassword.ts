2ff1897d8cdedb7fe6e94c7ce407915d8ce03f84
palmcm <palmermonkeyboy@gmail.com>
2021-12-25 03:56:43 +0700

    New extract password
    Cons: use many memory
    Pros: harder to brute force

--- core/extractPassword.ts
+++ core/extractPassword.ts
@@ -1,95 +1,32 @@
 import { RoomPasswordMapping } from "../components/PasswordList"
+import { encode } from "./encode"
+import { wordlist } from "./wordlist"
 
-const getCharacterFrom = (pairs: RoomPasswordMapping[], roomName: string, index: number): string => {
-    let targetPair = pairs.filter(pair => pair.room == "Room " + roomName)
-    if (targetPair.length < 1) return "not found room"
+const getIndexFrom = (encodeRoom:string, start: number, end:number):number => {
+    let ans = 0;
+    for (let i=start;i<end;i++){
+        ans += (i<encodeRoom.length ? encodeRoom[i].charCodeAt(0) : 0)
+    }
+    return ans;
+}
 
-    let pair = targetPair[0]
-    if (pair.value.length <= index) return ""
-
-    return pair.value[index]
+const getWordFrom = (pairs: RoomPasswordMapping[], index: number): string => {
+    const {room ,range} = encode[index];
+    const con = encode[index].const;
+    let ans = 0;
+    room.forEach((room:string, ind:number)=>{
+        let targetPair = pairs.filter(pair => pair.room == "Room " + room)
+        if (targetPair.length < 1) return ans+= con[ind];
+        ans+= getIndexFrom(targetPair[0].value, range[ind][0], range[ind][1])*con[ind]
+    })
+    return wordlist[ans%wordlist.length]
 }
 
 const extractPassword = (pairs: RoomPasswordMapping[]): string => {
     let keyPhrase: string = ""
-    if (pairs.length < 15) return ""
-
-    keyPhrase += getCharacterFrom(pairs, "X", 1)
-    keyPhrase += getCharacterFrom(pairs, "L", 2)
-    keyPhrase += getCharacterFrom(pairs, "F", 1)
-    keyPhrase += getCharacterFrom(pairs, "Bonus", 1)
-    keyPhrase += getCharacterFrom(pairs, "U", 3)
-    keyPhrase += getCharacterFrom(pairs, "S", 11)
-    keyPhrase += getCharacterFrom(pairs, "E", 13)
-    keyPhrase += getCharacterFrom(pairs, "R", 22)
-    keyPhrase += getCharacterFrom(pairs, "J", 8)
-    keyPhrase += getCharacterFrom(pairs, "R", 0)
-    keyPhrase += getCharacterFrom(pairs, "U", 7)
-    keyPhrase += getCharacterFrom(pairs, "V", 2)
-    keyPhrase += getCharacterFrom(pairs, "V", 0)
-    keyPhrase += getCharacterFrom(pairs, "X", 6)
-    keyPhrase += getCharacterFrom(pairs, "X", 6)
-    keyPhrase += getCharacterFrom(pairs, "R", 1)
-    keyPhrase += getCharacterFrom(pairs, "R", 8)
-    keyPhrase += getCharacterFrom(pairs, "X", 2)
-    keyPhrase += getCharacterFrom(pairs, "R", 3)
-    keyPhrase += getCharacterFrom(pairs, "R", 5)
-    keyPhrase += getCharacterFrom(pairs, "R", 5)
-    keyPhrase += getCharacterFrom(pairs, "R", 26)
-    keyPhrase += getCharacterFrom(pairs, "X", 7)
-    keyPhrase += getCharacterFrom(pairs, "R", 33)
-    keyPhrase += getCharacterFrom(pairs, "U", 7)
-    keyPhrase += getCharacterFrom(pairs, "A", 12)
-    keyPhrase += getCharacterFrom(pairs, "U", 6)
-    keyPhrase += getCharacterFrom(pairs, "S", 6)
-    keyPhrase += getCharacterFrom(pairs, "S", 2)
-    keyPhrase += getCharacterFrom(pairs, "R", 13)
-    keyPhrase += getCharacterFrom(pairs, "J", 19)
-    keyPhrase += getCharacterFrom(pairs, "R", 11)
-    keyPhrase += getCharacterFrom(pairs, "S", 2)
-    keyPhrase += getCharacterFrom(pairs, "E", 8)
-    keyPhrase += getCharacterFrom(pairs, "O", 1)
-    keyPhrase += getCharacterFrom(pairs, "O", 4)
-    keyPhrase += getCharacterFrom(pairs, "J", 10)
-    keyPhrase += getCharacterFrom(pairs, "A", 6)
-    keyPhrase += getCharacterFrom(pairs, "X", 10)
-    keyPhrase += getCharacterFrom(pairs, "R", 13)
-    keyPhrase += getCharacterFrom(pairs, "R", 16)
-    keyPhrase += getCharacterFrom(pairs, "U", 7)
-    keyPhrase += getCharacterFrom(pairs, "J", 20)
-    keyPhrase += getCharacterFrom(pairs, "R", 2)
-    keyPhrase += getCharacterFrom(pairs, "R", 25)
-    keyPhrase += getCharacterFrom(pairs, "J", 15)
-    keyPhrase += getCharacterFrom(pairs, "X", 5)
-    keyPhrase += getCharacterFrom(pairs, "R", 46)
-    keyPhrase += getCharacterFrom(pairs, "X", 21)
-    keyPhrase += getCharacterFrom(pairs, "L", 5)
-    keyPhrase += getCharacterFrom(pairs, "B", 6)
-    keyPhrase += getCharacterFrom(pairs, "R", 29)
-    keyPhrase += getCharacterFrom(pairs, "O", 8)
-    keyPhrase += getCharacterFrom(pairs, "A", 9)
-    keyPhrase += getCharacterFrom(pairs, "R", 30)
-    keyPhrase += getCharacterFrom(pairs, "E", 11)
-    keyPhrase += getCharacterFrom(pairs, "J", 3)
-    keyPhrase += getCharacterFrom(pairs, "L", 17)
-    keyPhrase += getCharacterFrom(pairs, "U", 2)
-    keyPhrase += getCharacterFrom(pairs, "R", 22)
-    keyPhrase += getCharacterFrom(pairs, "V", 10)
-    keyPhrase += getCharacterFrom(pairs, "U", 5)
-    keyPhrase += getCharacterFrom(pairs, "R", 17)
-    keyPhrase += getCharacterFrom(pairs, "S", 6)
-    keyPhrase += getCharacterFrom(pairs, "J", 4)
-    keyPhrase += getCharacterFrom(pairs, "O", 10)
-    keyPhrase += getCharacterFrom(pairs, "J", 10)
-    keyPhrase += getCharacterFrom(pairs, "R", 34)
-    keyPhrase += getCharacterFrom(pairs, "R", 22)
-    keyPhrase += getCharacterFrom(pairs, "J", 2)
-    keyPhrase += getCharacterFrom(pairs, "R", 20)
-    keyPhrase += getCharacterFrom(pairs, "V", 3)
-    keyPhrase += getCharacterFrom(pairs, "L", 2)
-    keyPhrase += getCharacterFrom(pairs, "O", 3)
-    keyPhrase += getCharacterFrom(pairs, "H", 0)
-    keyPhrase += getCharacterFrom(pairs, "O", 8)
+    for (let i=0;i<encode.length;i++){
+        keyPhrase += getWordFrom(pairs, i) + " ";
+    }
 
     return keyPhrase.toLowerCase()
 }
